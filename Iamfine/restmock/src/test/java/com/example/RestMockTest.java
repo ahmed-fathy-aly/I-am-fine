7d7ed516e749b5cbd7e2dbeb98dbba490d4e4bb7
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2017-02-03 17:56:26 +0100

    init project

---
+++ Iamfine/restmock/src/test/java/com/example/RestMockTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class RestMockTest {
+    
+    @Test
+    public void sanityTest(){
+        assertEquals(3, 3);
+    }
+}
