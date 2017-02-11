41aef5594fe4e03036352c515598072109e38253
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2017-02-11 19:24:01 +0100

    add main screen presenter

---
+++ Iamfine/app/src/main/java/com/enterprises/wayne/iamfine/main_screen/UserDataInteractor.java
@@ -0,0 +1,29 @@
+package com.enterprises.wayne.iamfine.main_screen;
+
+import com.enterprises.wayne.iamfine.base.BaseNetworkCallback;
+import com.enterprises.wayne.iamfine.main_screen.view_model.UserViewModel;
+
+import java.util.List;
+
+/**
+ * Created by Ahmed on 2/11/2017.
+ */
+
+public interface UserDataInteractor {
+
+    void getRecommendedUsers(GetRecommendedUsersCallback callback);
+
+    interface GetRecommendedUsersCallback extends BaseNetworkCallback{
+        void recommendedUsers(List<UserViewModel> users);
+
+        void noneRecommended();
+    }
+
+    void searchUsers(String abc, SearchUsersCallback callback);
+
+    interface SearchUsersCallback extends BaseNetworkCallback{
+        void foundUsers(List<UserViewModel> users);
+
+        void noneFound();
+    }
+}
