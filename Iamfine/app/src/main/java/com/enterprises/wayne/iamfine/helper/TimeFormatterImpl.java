dfd2ad208fc879c50f2e33d3c21665f5683b7972
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2017-02-18 19:59:26 +0100

    add time formatter to display time prettier

---
+++ Iamfine/app/src/main/java/com/enterprises/wayne/iamfine/helper/TimeFormatterImpl.java
@@ -0,0 +1,47 @@
+package com.enterprises.wayne.iamfine.helper;
+
+/**
+ * shows time string like "4 minutes ago"
+ * Created by Ahmed on 2/18/2017.
+ */
+public class TimeFormatterImpl implements TimeFormatter {
+
+    @Override
+    public String getDisplayTime(long timeInMillis) {
+        long curr = System.currentTimeMillis();
+        long diff = curr - timeInMillis;
+
+        // less than a minute
+        long SECOND = 1000l;
+        long MINUTE = 60l * SECOND;
+        if (diff <= 60 * 1000)
+            return "Just now";
+
+        // less than an hour
+        long HOUR = 60l * MINUTE;
+        if (diff <= HOUR) {
+            int nMinutes = (int) (diff / MINUTE);
+            if (nMinutes > 1)
+                return nMinutes + " minutes ago";
+            else
+                return "a minute ago";
+        }
+
+        // less than a day
+        long DAY = 24l * HOUR;
+        if (diff <= DAY) {
+            int nHours = (int) (diff / HOUR);
+            if (nHours > 1)
+                return nHours + " hours ago";
+            else
+                return "an hour ago";
+        }
+
+        // many day
+        int nDays = (int) (diff / DAY);
+        if (nDays > 1)
+            return nDays + " days ago";
+        else
+            return "yesterday";
+    }
+}
---
+++ Iamfine/app/src/test/java/com/enterprises/wayne/iamfine/helper/TimeFormatterImplTest.java
@@ -0,0 +1,62 @@
+package com.enterprises.wayne.iamfine.helper;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Ahmed on 2/18/2017.
+ */
+public class TimeFormatterImplTest {
+
+    private TimeFormatterImpl timeFormmatter;
+
+    @Before
+    public void setup() {
+        timeFormmatter = new TimeFormatterImpl();
+    }
+
+    @Test
+    public void testJustNow() {
+        long time = System.currentTimeMillis() - 10 *1000;
+        Assert.assertEquals("Just now", timeFormmatter.getDisplayTime(time));
+    }
+
+    @Test
+    public void testAMinuteAGo() {
+        long time = System.currentTimeMillis() - 1*60*1000 - 30;
+        Assert.assertEquals("a minute ago", timeFormmatter.getDisplayTime(time));
+    }
+
+
+    @Test
+    public void testMinutesAgo() {
+        long time = System.currentTimeMillis() - 15*60*1000 - 30;
+        Assert.assertEquals("15 minutes ago", timeFormmatter.getDisplayTime(time));
+    }
+
+    @Test
+    public void testAnHourAgo() {
+        long time = System.currentTimeMillis() - 60*60*1000 - 30;
+        Assert.assertEquals("an hour ago", timeFormmatter.getDisplayTime(time));
+    }
+
+    @Test
+    public void testHoursAgo() {
+        long time = System.currentTimeMillis() - 15*60*60*1000 - 30;
+        Assert.assertEquals("15 hours ago", timeFormmatter.getDisplayTime(time));
+    }
+
+    @Test
+    public void testADayAgo() {
+        long time = System.currentTimeMillis() - 24*60*60*1000 - 30;
+        Assert.assertEquals("yesterday", timeFormmatter.getDisplayTime(time));
+    }
+
+    @Test
+    public void testDaysAgo() {
+        long time = System.currentTimeMillis() - 15*24*60*60*1000 - 30;
+        Assert.assertEquals("15 days ago", timeFormmatter.getDisplayTime(time));
+    }
+}
