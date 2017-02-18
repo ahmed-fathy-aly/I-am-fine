a626a3ff4d47247d2a28f95759d206ac6288af06
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2017-02-18 19:45:28 +0100

    add dummy remote data sources mocking the backend

---
+++ Iamfine/app/src/main/java/com/enterprises/wayne/iamfine/repo/RemoteUserDataRepoImpl.java
@@ -0,0 +1,31 @@
+package com.enterprises.wayne.iamfine.repo;
+
+import com.enterprises.wayne.iamfine.data_model.UserDataModel;
+import com.enterprises.wayne.iamfine.exception.NetworkErrorException;
+import com.enterprises.wayne.iamfine.exception.UnKnownErrorException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * returns dummy data till it's connected to the backend
+ * Created by Ahmed on 2/18/2017.
+ */
+public class RemoteUserDataRepoImpl implements RemoteUserDataRepo {
+    @Override
+    public List<UserDataModel> searchUsers(String searchStr) throws NetworkErrorException, UnKnownErrorException {
+        List<UserDataModel> users = new ArrayList<>();
+        for (int i = 0; i < 5; i++)
+            users.add(new UserDataModel("id" + i, searchStr + " hamada", "hamada@gmail.com", "", System.currentTimeMillis() - (i + 1) * 100000));
+        return users;
+    }
+
+    @Override
+    public List<UserDataModel> getSuggestedUsers() throws NetworkErrorException, UnKnownErrorException {
+        return Arrays.asList(
+                new UserDataModel("1", "Hamada", "hamada@gmail.com", "", System.currentTimeMillis() - 100000),
+                new UserDataModel("2", "Aba", "aba@gmail.com", "", System.currentTimeMillis() - 200000),
+                new UserDataModel("3", "Yara", "yara@gmail.com", "", System.currentTimeMillis() - 400000));
+    }
+}
---
+++ Iamfine/app/src/main/java/com/enterprises/wayne/iamfine/repo/RemoteWhoAskedRepoImpl.java
@@ -0,0 +1,27 @@
+package com.enterprises.wayne.iamfine.repo;
+
+import com.enterprises.wayne.iamfine.data_model.UserDataModel;
+import com.enterprises.wayne.iamfine.data_model.WhoAskedDataModel;
+import com.enterprises.wayne.iamfine.exception.NetworkErrorException;
+import com.enterprises.wayne.iamfine.exception.UnKnownErrorException;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * returns dummy data till it's connected to the backend
+ * Created by Ahmed on 2/18/2017.
+ */
+public class RemoteWhoAskedRepoImpl implements RemoteWhoAskedRepo {
+    @Override
+    public List<WhoAskedDataModel> getWhoAsked() throws NetworkErrorException, UnKnownErrorException {
+        return Arrays.asList(
+                new WhoAskedDataModel(
+                        new UserDataModel("1", "Hamada", "hamada@gmail.com", "", System.currentTimeMillis() - 100000),
+                        System.currentTimeMillis() - 100000),
+                new WhoAskedDataModel(new UserDataModel("2", "Aba", "aba@gmail.com", "", System.currentTimeMillis() - 200000),
+                        System.currentTimeMillis() - 200000),
+                new WhoAskedDataModel(new UserDataModel("3", "Yara", "yara@gmail.com", "", System.currentTimeMillis() - 400000),
+                        System.currentTimeMillis() - 400000));
+    }
+}
